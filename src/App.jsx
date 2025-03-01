6108d8a98150f832ad15dfc4b1c480bc04140a6b
Preet Singhwal <preetsinghwal38@gmail.com>
2025-03-01 01:42:36 -0800

    setup styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30pxl;
+  font-weight: 700l; 
+`;
 
 function App() {
 
   return (
-    <div>Hellow World</div>
+    <>
+      <GlobalStyles />|
+      <div>
+        <H1>The wild Oasis</H1>
+      </div>
+    </>
   )
 }
 
